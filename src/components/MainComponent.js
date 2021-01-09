7bf5fae6af225854032559d62f0c2fbf5f204024
Darren Diggs <darrendiggs00@gmail.com>
2021-01-08 19:29:44 -0600

    React Redux Form & Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,7 +9,6 @@ import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
