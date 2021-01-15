e44f435a77bdf0e60d31f61318189c1edf880d0c
Darren Diggs <darrendiggs00@gmail.com>
2021-01-15 14:04:52 -0600

    React Animation Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -9,6 +9,7 @@ import React, { Component } from 'react';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -133,15 +134,23 @@ function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col.md-5 m-1">
-                <h4>Comments</h4>
-                {comments.map(comm =>
-                    <div key={comm.id}>
-                        "{comm.text}"
-                      <br></br>
-                      - {comm.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comm.date)))}
-                        <hr></hr>
-                    </div>
-                )}
+                      <h4>Comments</h4>
+                <Stagger in>
+                    {
+                        comments.map(comment => {
+                            return (
+                                <Fade in key={comment.id}>
+                                    <div>
+                                        <p>
+                                            {comment.text}<br />
+                                            -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                        </p>
+                                    </div>
+                                </Fade>
+                            );
+                        })
+                    }
+                </Stagger>
                 <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         )
@@ -154,13 +163,19 @@ function RenderComments({comments, postComment, campsiteId}) {
 function RenderCampsite({ campsite }) {
     return (
         <div className="col-md-5 m-1">
+        <FadeTransform
+            in
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(-50%)'
+            }}>
             <Card>
-            <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
             </Card>
-        </div>
+        </FadeTransform>
+    </div>
     );
 }
 
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -4,8 +4,9 @@ import { Loading } from './LoadingComponent';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import React from 'react';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform } from 'react-animation-components';
 
-function RenderCard({item, isLoading, errMess}) {
+function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
         return <Loading />;
     }
@@ -13,13 +14,17 @@ function RenderCard({item, isLoading, errMess}) {
         return <h4>{errMess}</h4>;
     }
     return (
-        <Card>
-             <CardImg src={baseUrl + item.image} alt={item.name} />
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
+        <FadeTransform in transformProps={{
+            exitTransform: 'scale(0.5) translateY(50%)'
+        }}>
+            <Card>
+                <CardImg src={baseUrl + item.image} alt={item.name} />
+                <CardBody>
+                    <CardTitle>{item.name}</CardTitle>
+                    <CardText>{item.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     );
 }
 
@@ -28,8 +33,8 @@ function Home(props) {
         <div className="container">
             <div className="row">
                 <div className="col-md m-1">
-                    
-                <RenderCard
+
+                    <RenderCard
                         item={props.campsite}
                         isLoading={props.campsitesLoading}
                         errMess={props.campsitesErrMess}
@@ -38,7 +43,7 @@ function Home(props) {
                 </div>
                 <div className="col-md m-1">
 
-                <RenderCard
+                    <RenderCard
                         item={props.promotion}
                         isLoading={props.promotionLoading}
                         errMess={props.promotionErrMess}
